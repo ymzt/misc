f6d4c8ca6f3a541aedfdae41c3ede03855ff15b9
ymzt <ymzt@users.noreply.github.com>
2021-05-04 18:41:37 +0900

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,3 +1,11 @@
+_color () {
+    local c=$1;shift
+    echo -e "\e[38;5;${c}m$*\e[0m"
+}
+
+_log () {
+  _color 148 $(date) "$@"
+}
 
 pkg install -y git
 pkg install termux-api
@@ -7,3 +15,5 @@ echo "[$(date)] install"
 pkg install -y curl 
 curl -OL https://raw.githubusercontent.com/BuildAPKs/buildAPKs/master/setup.buildAPKs.bash
 bash setup.buildAPKs.bash
+
+pkg install python
