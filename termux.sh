7f368434fd896c769520b86df5a7853e4356cc9f
ymzt <ymzt@users.noreply.github.com>
2021-05-04 18:25:55 +0900

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,1 +1,9 @@
+
 pkg install -y git
+pkg install termux-api
+
+#---
+echo "[$(date)] install"
+pkg install -y curl 
+curl -OL https://raw.githubusercontent.com/BuildAPKs/buildAPKs/master/setup.buildAPKs.bash
+bash setup.buildAPKs.bash
